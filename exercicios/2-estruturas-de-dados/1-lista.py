f2e20c5eeecb2b3bc8156ca68741efdfefaf0bbb
ikibranchini <ikibranchini@gmail.com>
2024-12-27 20:55:59 +0000

    lista

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,29 @@
 # Crie uma lista apenas com elementos numéricos
 
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
+
+pessoa = [[1, 2, 3, 4],['Henrique', 'Julia', 'Gabriel'],True, False, ['Cristiane', 36, 2010]]
 
 # Imprima na tela apenas os 5 primeiros elementos da lista
 
+pessoa [:5]
+
 # Crie um slice na lista para que imprima na tela os elementos de índice par
+
+lista [0:-1:2]
 
 # Remova da lista o último item
 
+pessoa.pop ()
+
 # Insira na lista um novo item
 
+pessoa.append ('Filosofia')
+print(pessoa)
+
 # Remova da lista um item específico
+
+pessoa.remove ('Cristiane')
+print(lista)
