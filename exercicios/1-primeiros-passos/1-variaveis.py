87150330f96f3fcd0243dfc371f14501b622ab9b
ikibranchini <ikibranchini@gmail.com>
2024-12-27 20:21:47 +0000

    variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,27 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'henrique'
+
+profissao ="programador"
+
+objetivo = '''
+Aprender Python
+para mudar
+de Profissao.
+'''
 
 # Declare variáveis de cada tipo numérico
 
+numero_inteiro = 5
+
+numero_float = 2.7
+
+numero_complex = 2+1j
+
 
 # Declare uma variável do tipo booleano
+
+meu_cachorro_comeu_toda_a_raçao = True
+
+meu_gato_comeu_toda_a_raçao = False
